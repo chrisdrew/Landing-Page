4f4b30b49d6b1acff850bbd5139e8794f00f5b84
chris <chris@republicbjj.com>
2020-03-28 14:59:46 -0600

    added scrolling effect

--- js/app.js
+++ js/app.js
@@ -30,6 +30,7 @@ const navItems = document.getElementsByTagName('section');
 */
 let navbar = document.getElementById('navbar__list');
 let navID, navName;
+let navArray = new Array();
 /**
  * End Global Variables
  * Start Helper Functions
@@ -45,29 +46,50 @@ let navID, navName;
 */
 
 // build the nav
+// ************************
+// clean up code later
+// ************************
+
 function buildNav() {
     for (let i = 0; i <= navItems.length - 1; i++) {
         if (navItems[i].hasAttribute('id')) {
             navID = navItems[i].getAttribute('id');
             navName = navItems[i].getAttribute('data-nav');
-            
             const templateNav = `
                 <li>
-                <a href="#${navID}" class="menu__link">
+                <a href="#${navID}" class="menu__link" id="section_${i+1}">
                     ${navName}
                 </a>
             </li>
             `;
+            navArray.push(templateNav);
             navbar.insertAdjacentHTML('beforeEnd', templateNav);
         }
     }
     return
 }
 // Add class 'active' to section when near top of viewport
-
+function addActive(item){
+    const sections = document.getElementsByTagName('section');
+    for (let section of sections) {
+        if(section.classList.contains('your-active-class')){
+            section.classList.remove('your-active-class');
+        }
+    }
+    item.classList.add('your-active-class');
+    return
+ 
+}
 
 // Scroll to anchor ID using scrollTO event
-
+function scrollTo(event){
+    event.preventDefault();
+    const scrollString = event.target.hash.substring(1);
+    const scrollTo = document.getElementById(scrollString);
+    scrollTo.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+    addActive(scrollTo);
+    return
+}
 
 /**
  * End Main Functions
@@ -79,5 +101,17 @@ function buildNav() {
 buildNav();
 
 // Scroll to section on link click
+const buttons = document.querySelectorAll('a');
+for (const button of buttons) {
+    button.addEventListener('click', function(event) {
+        event.preventDefault();
+        const scrollString = event.target.hash.substring(1);
+        const scrollTo = document.getElementById(scrollString);
+        scrollTo.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+        addActive(scrollTo);
+        return
+    })
+}
+
 
 // Set sections as active
